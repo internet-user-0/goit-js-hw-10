54f15bc24827c45884fbb99fd86df0c868a52b63
Anton Zvertanovsky <anton.zv2002@gmail.com>
2022-10-25 00:23:07 +0300

    revision

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,15 @@
 export function fetchCountries(name) {
-   const BASE_URL = "https://restcountries.com/v3.1/"
-   return fetch(
-     `${BASE_URL}name/${name}?`
-   )
-     .then(response => {        
-         if (response.status === 200) {            
-             return response.json();
-           }
-                 
-           if (response.status === 404) {
-             return Promise.reject('Error 404');
-           }     
-     })    
- } 
+  const BASE_URL = "https://restcountries.com/v3.1/"
+  return fetch(
+    `${BASE_URL}name/${name}?`
+  )
+    .then(response => {
+        if (response.status === 200) {
+            return response.json();
+          }
+
+          if (response.status === 404) {
+            return Promise.reject('Error 404');
+          }
+    })
+}
